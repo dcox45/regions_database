005bd1b17a53fd6907c59506c78eabb2a5a4a7bd
Dylan Cox <dylanwjc@gmail.com>
2018-04-26 17:51:22 -0400

    added sign in button to nav

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -5,7 +5,7 @@ class RegionsController < ApplicationController
   def home
     @regions = Region.all.order('begDate')  # RECENTLY ADDED ".ALL"
     @years = ((Date.today - @regions.first.begDate) / 365).to_i    #"Date.today" replaced longer form (@region.last.endDate)
-    @days = (Date.today - @regions.first.begDate).to_i 
+    @days = (Date.today - @regions.first.begDate).to_i
     render('!home.html.erb')
   end
 
