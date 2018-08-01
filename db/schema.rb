29811f572eb753d837a259fb23583fa24c503a65
Dylan Cox <dylanwjc@gmail.com>
2018-08-01 13:56:34 -0400

    basic generate and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180324013326) do
+ActiveRecord::Schema.define(version: 20180801171837) do
 
   create_table "locations", force: :cascade do |t|
     t.string   "caption"
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 20180324013326) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.string   "description"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
