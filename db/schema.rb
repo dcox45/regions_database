3e735ad8a65b93600e8f8d80ecf128a8ad17dbab
Dylan Cox <dylanwjc@gmail.com>
2018-08-01 17:55:07 -0400

    finished basic User model w errors persisting

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180801171837) do
+ActiveRecord::Schema.define(version: 20180801205627) do
 
   create_table "locations", force: :cascade do |t|
     t.string   "caption"
@@ -41,8 +41,10 @@ ActiveRecord::Schema.define(version: 20180801171837) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
