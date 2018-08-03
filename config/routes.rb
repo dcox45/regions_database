d9de9c95f806f43fc431b25b66fe1c775db133e1
Dylan Cox <dylanwjc@gmail.com>
2018-08-02 21:15:58 -0400

    Revert "Modelling users"

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   get 'users/new'
 
   root "regions#home"
 
   #Users
   get  '/signup',  to: 'users#new'
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
 
   #REGIONS
   get('/regions/:name', {:controller => 'regions', :action => 'show'})
