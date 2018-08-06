5e7eafad6caa280cdfafec84287775551334a7a7
Dylan Cox <dylanwjc@gmail.com>
2018-08-06 13:51:40 -0400

    fixing links to location on region show page

--- config/routes.rb
+++ config/routes.rb
@@ -11,22 +11,26 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
-  #REGIONS
-  get('/regions/:name', {:controller => 'regions', :action => 'show'})
-  get('/new_region', {:controller => 'regions', :action => 'new'})
-  get('/create_region', {:controller => 'regions', :action => 'create'})
-  get('/:name/edit', {:controller => 'regions', :action => 'edit'})
-  get('/update_region/:name', {:controller => 'regions', :action => 'update'})
-  get('/destroy_region/:name', {:controller => 'regions', :action => 'destroy'})
+resources :regions, param: :name do
+  resources :locations
+end
 
-  #LOCATIONS
-  get('/regions/:name/:id', {:controller => 'locations', :action => 'show'})
-  get('/:name/new_location', {:controller => 'locations', :action => 'new'})
-  get('/:name/create_location', {:controller => 'locations', :action => 'create'})
-  get('/:name/:id/edit', {:controller => 'locations', :action => 'edit'})
-  get('/:name/update_location/:id', {:controller => 'locations', :action => 'update'})
-  get('/:name/destroy_location/:id', {:controller => 'locations', :action => 'destroy'})
-  get('/all_locations', {:controller => 'locations', :action => 'index'})
+  # #REGIONS
+  # get('/regions/:name', {:controller => 'regions', :action => 'show'})
+  # get('/new_region', {:controller => 'regions', :action => 'new'})
+  # get('/create_region', {:controller => 'regions', :action => 'create'})
+  # get('/:name/edit', {:controller => 'regions', :action => 'edit'})
+  # get('/update_region/:name', {:controller => 'regions', :action => 'update'})
+  # get('/destroy_region/:name', {:controller => 'regions', :action => 'destroy'})
+  #
+  # #LOCATIONS
+  # get('/regions/:name/:id', {:controller => 'locations', :action => 'show'})
+  # get('/:name/new_location', {:controller => 'locations', :action => 'new'})
+  # get('/:name/create_location', {:controller => 'locations', :action => 'create'})
+  # get('/:name/:id/edit', {:controller => 'locations', :action => 'edit'})
+  # get('/:name/update_location/:id', {:controller => 'locations', :action => 'update'})
+  # get('/:name/destroy_location/:id', {:controller => 'locations', :action => 'destroy'})
+  # get('/all_locations', {:controller => 'locations', :action => 'index'})
 
   #WARNINGS
   get('/destroy_warning/:name', {:controller => 'regions', :action => 'warning'})
